a91dbde413e3c5d40e81ce2fd8a66d4132cbfb32
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-05-16 13:01:25 +0530

    🚀 RELEASE: day_0 and day_1

---
+++ day_0.js
@@ -0,0 +1,51 @@
+// let data = null;
+// let data_1;
+
+// console.log(typeof data, typeof data_1);
+
+// let str = null;
+// let str_1;
+// str_1 = "";
+
+// str = "d";
+
+// console.log(str, str_1);
+
+// type conversion
+
+// let data = true;
+// console.log(typeof data, data);
+
+// data = Number(data);
+// console.log(typeof data, data);
+
+// data = String(data);
+// console.log(typeof data, data);
+
+// data = Boolean(data);
+// console.log(typeof data, data);
+
+// logical operators
+
+// or operator
+
+// let a = false;
+// let b = false;
+// let c = true;
+
+// console.log(a || b || c);
+
+// and operator
+
+// let a = true;
+// let b = true;
+
+// console.log(a && b);
+
+// not operator
+
+let a = true;
+let b = false;
+
+console.log(!a);
+console.log(!b);
---
+++ day_1.js
@@ -0,0 +1,69 @@
+// let a = 5,
+//   b = 2;
+
+// operation unary operator
+
+// addition
+// console.log(a + b);
+
+// // subtraction
+// console.log(a - b);
+
+// // multiplication
+// console.log(a * b);
+
+// // division
+// console.log(a / b);
+
+// // reminder operator / modulus operator
+
+// console.log(a % b);
+
+// // exponential operator
+
+// console.log(a ** b);
+
+// binary plus operator
+
+// let a = 10,
+//   b = "3";
+
+// console.log(a + b);
+
+// console.log(a - b);
+
+// console.log(a * b);
+
+// console.log(a / b);
+
+// console.log(a % b);
+
+// console.log(a ** b);
+
+// console.log(a + +b);
+
+// comparison operator
+
+// let a = 10,
+//   b = "11";
+
+// console.log(a < b);
+
+// if statement
+
+// let a = 70,
+//   b = 80;
+
+// if (a > b) {
+//   console.log("a greater than b");
+// }
+
+// if (a > b) {
+//   console.log("a greater than b");
+// } else {
+//   console.log("a less than b");
+// }
+
+// optional operator
+
+// console.log(a > b ? "a greater than b" : "a less than b");
